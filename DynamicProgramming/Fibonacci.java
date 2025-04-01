09faf653648ba5c8ff841ffe0f5089ed14cc7bd6
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2025-04-01 22:38:40 +0530

    dp updated

---
+++ DynamicProgramming/Fibonacci.java
@@ -0,0 +1,22 @@
+package java_BASIC_OOPS_DSA_PROJECTS_PROBLEMS.DynamicProgramming;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static void main(String args[]) {
+
+        int n=5;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(f(n,dp));
+
+    }
+
+    static int f(int n, int[] dp){
+        if(n<=1) return n;
+
+        if(dp[n]!= -1) return dp[n];
+        return dp[n]= f(n-1,dp) + f(n-2,dp);
+    }
+
+}
