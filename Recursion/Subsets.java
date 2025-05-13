b1338fb3d54c077985edb80c7357404631edd568
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2025-05-13 13:35:24 +0530

    updated

---
+++ Recursion/Subsets.java
@@ -0,0 +1,33 @@
+package java_BASIC_OOPS_DSA_PROJECTS_PROBLEMS.Recursion;
+
+import java.util.ArrayList;
+
+public class Subsets {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3};
+        System.out.println(subsets(nums));
+
+    }
+
+
+    public static ArrayList<ArrayList<Integer>> subsets(int[] nums) {
+
+        ArrayList<ArrayList<Integer>> a = new ArrayList<ArrayList<Integer>>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            int count = 1;
+            for(int j=0 ; j < count ; j++){
+
+            }
+
+        }
+
+
+
+
+
+        return a;
+    }
+}
---
+++ String/Problems.java
@@ -0,0 +1,24 @@
+package java_BASIC_OOPS_DSA_PROJECTS_PROBLEMS.String;
+
+public class Problems {
+
+    public static void main(String[] args) {
+
+        countDigit(12345);
+        stringReverse(123567);
+    }
+
+    static void countDigit(int a){
+        String b = Integer.toString(a);
+        System.out.println(b.length());
+    }
+
+    static void stringReverse(int a){
+
+        StringBuffer str = new StringBuffer(String.valueOf(a));
+        str.reverse();
+        int b = Integer.parseInt(String.valueOf(str));
+        System.out.println(b);
+
+    }
+}
