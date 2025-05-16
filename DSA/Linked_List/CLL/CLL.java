17ab3f5944c7ba03fd78f261c615c5240d70e39a
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2025-05-16 23:13:07 +0530

    updated

---
+++ DSA/Linked_List/CLL/CLL.java
@@ -0,0 +1,69 @@
+package Java_Bootcamp.DSA.Linked_List.CLL;
+
+public class CLL {
+
+    private CLLNode head;
+    private CLLNode tail;
+
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int val) {
+        CLLNode node = new CLLNode(val);
+        if (head == null) {
+            head = node;
+            tail = node;
+            node.next = head;
+            return;
+        }
+
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+
+    public void display() {
+        CLLNode node = head;
+        if (head != null) {
+            do {
+                System.out.print(node.val + " -> ");
+                node = node.next;
+            } while (node != head);
+        }
+        System.out.println("HEAD");
+    }
+
+    public void delete(int val) {
+        CLLNode node = head;
+        if (node == null) {
+            return;
+        }
+
+        if (head == tail && head.val == val) {
+            head = null;
+            tail = null;
+            return;
+        }
+
+        if (head.val == val) {
+            head = head.next;
+            tail.next = head;
+            return;
+        }
+
+        do {
+            CLLNode nextNode = node.next;
+            if (nextNode.val == val) {
+                node.next = nextNode.next;
+                if (nextNode == tail) {
+                    tail = node;
+                }
+                break;
+            }
+            node = node.next;
+        } while (node != head);
+    }
+}
+
---
+++ DSA/Linked_List/CLL/CLLNode.java
@@ -0,0 +1,10 @@
+package Java_Bootcamp.DSA.Linked_List.CLL;
+
+public class CLLNode {
+    int val;
+    CLLNode next;
+
+    public CLLNode(int val) {
+        this.val = val;
+    }
+}
